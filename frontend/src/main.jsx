38bd71ca284afeba2db71b1db02ab5189bf09159
Eneias da Fonseca Dias <edias.dias@terra.com.br>
2025-04-16 22:15:49 -0300

    ✅ Adiciona o registerSW para ativar o PWA

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'  // <--- Isso precisa estar aqui
+import './index.css'
+
+// 🔥 Registro do Service Worker do Vite PWA
+import { registerSW } from 'virtual:pwa-register'
+registerSW()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
